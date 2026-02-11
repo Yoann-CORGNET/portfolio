ef831b7c7a43a9f4549be904045c69897edace74
Yoann CORGNET <ycorgnet@et.esiea.fr>
2026-02-11 20:07:08 +0100

    hotfix(vercel): use correct env var for url

--- src/lib/get-base-url.ts
+++ src/lib/get-base-url.ts
@@ -1,15 +1,14 @@
-export function getBaseUrl(): string {
+export function getBaseUrl() {
+  // Browser -> use relative path
+  if (typeof window !== "undefined") {
+    return ""
+  }
 
-  // Custom domain URL
-  if (process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL)
-    return `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`;
+  // Vercel (production || preview)
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`
+  }
 
-  if (process.env.NEXT_PUBLIC_APP_URL) 
-    return process.env.NEXT_PUBLIC_APP_URL;
-  
-  // Default Vercel generated deployment URL
-  if (process.env.NEXT_PUBLIC_VERCEL_URL) 
-    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
-  
-  return "http://localhost:3000";
+  // Local development
+  return "http://localhost:3000"
 }
